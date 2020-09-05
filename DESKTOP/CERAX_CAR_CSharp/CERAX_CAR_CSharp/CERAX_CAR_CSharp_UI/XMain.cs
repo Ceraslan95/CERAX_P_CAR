8ae67544f017ae91bdea7713e4b290576bead354
Ceraslan <43091334+Ceraslan95@users.noreply.github.com>
2020-09-05 17:23:09 +0300

    b

---
+++ DESKTOP/CERAX_CAR_CSharp/CERAX_CAR_CSharp/CERAX_CAR_CSharp_UI/XMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CERAX_CAR_CSharp_UI
+{
+    public partial class XMain : Form
+    {
+        public XMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
